14a10b9a5062ba5592d325a67c9db4dad8d174ea
Rafael Ribeiro <anthonyribeiro1910@gmail.com>
2024-11-12 11:51:12 -0300

    feat: testing get update and remove methods

--- api/src/users/users.service.spec.ts
+++ api/src/users/users.service.spec.ts
@@ -53,9 +53,35 @@ describe('UsersService', () => {
             password: 'glaucio1234',
           });
         }),
-        findUnique: (() => { }),
-        update: (() => { }),
-        delete: (() => { }),
+        findUnique: ((args) => {
+
+          if (args.where.id === 1) {
+            return Promise.resolve({
+              id: 1,
+              name: 'Rafael',
+              email: 'rafael@fiap.com.br',
+              password: 'Rafael1234',
+            });
+          } else {
+            return Promise.resolve(null);
+          }
+
+        }),
+        update: ((args) => {
+
+          return Promise.resolve({
+            ...args.where,
+            ...args.data,
+          });
+
+        }),
+        delete: ((args) => {
+
+          return Promise.resolve({
+            ...args.where,
+          });
+
+        }),
       }
     } as unknown as PrismaService;
 
@@ -106,6 +132,229 @@ describe('UsersService', () => {
         expect(error.response).toHaveProperty('message', 'Já há um usuário com este email');
         expect(error.response).toHaveProperty('error', 'Bad Request');
         expect(error.response).toHaveProperty('statusCode', 400);
+
+      }
+
+    });
+
+    it('should return an error if name is not provided', async () => {
+
+      try {
+
+        await service.create('', '', '');
+
+      } catch (error) {
+
+        expect(error).toBeDefined();
+        expect(error).toHaveProperty('response');
+        expect(error.response).toHaveProperty('message', 'Informe o nome do usuário');
+        expect(error.response).toHaveProperty('error', 'Bad Request');
+        expect(error.response).toHaveProperty('statusCode', 400);
+
+      }
+
+    });
+
+    it('should return an error if email is not provided', async () => {
+
+      try {
+
+        await service.create('Rafael', '', '');
+
+      } catch (error) {
+
+        expect(error).toBeDefined();
+        expect(error).toHaveProperty('response');
+        expect(error.response).toHaveProperty('message', 'Informe o email do usuário');
+        expect(error.response).toHaveProperty('error', 'Bad Request');
+        expect(error.response).toHaveProperty('statusCode', 400);
+
+      }
+
+    });
+
+    it('should return an error if password is not provided', async () => {
+
+      try {
+
+        await service.create('Rafael', 'rafael@fiap.com.br', '');
+
+      } catch (error) {
+
+        expect(error).toBeDefined();
+        expect(error).toHaveProperty('response');
+        expect(error.response).toHaveProperty('message', 'Informe a senha do usuário');
+        expect(error.response).toHaveProperty('error', 'Bad Request');
+        expect(error.response).toHaveProperty('statusCode', 400);
+
+      }
+
+    });
+
+  });
+
+  describe('get method', () => {
+
+    it('should return an user', async () => {
+
+      const user = await service.get(1);
+
+      expect(user.id).toBeDefined();
+      expect(user.name).toBeDefined();
+      expect(user.email).toBeDefined();
+      expect(user.password).toBeDefined();
+
+    });
+
+    it('should return an error if id is invalid', async () => {
+
+      try {
+
+        await service.get('ABC' as unknown as number);
+
+      } catch (error) {
+
+        expect(error).toBeDefined();
+        expect(error).toHaveProperty('response');
+        expect(error.response).toHaveProperty('message', 'ID inválido.');
+        expect(error.response).toHaveProperty('error', 'Bad Request');
+        expect(error.response).toHaveProperty('statusCode', 400);
+
+      }
+
+    });
+
+    it('should return an error if user is not found', async () => {
+
+      try {
+
+        await service.get(5);
+
+      } catch (error) {
+
+        expect(error).toBeDefined();
+        expect(error).toHaveProperty('response');
+        expect(error.response).toHaveProperty('message', 'Usuário não encontrado.');
+        expect(error.response).toHaveProperty('error', 'Not Found');
+        expect(error.response).toHaveProperty('statusCode', 404);
+
+      }
+
+    });
+
+  });
+
+  describe('update method', () => {
+
+    const name = 'Rafael Ribeiro';
+    const email = 'rafaelribeiro@fiap.com.br';
+    const password = 'Rafael123456';
+
+    it('should update an user', async () => {
+
+      const id = 1;
+
+      const updatedUser = await service.update(id, {
+        name: name,
+        email: email,
+        password: password,
+      });
+
+      expect(updatedUser).toHaveProperty('id', id);
+      expect(updatedUser).toHaveProperty('name', name);
+      expect(updatedUser).toHaveProperty('email', email);
+      expect(updatedUser).toHaveProperty('password', password);
+
+    });
+
+    it('should return an error if id is invalid', async () => {
+
+      try {
+
+        await service.update('ABC' as unknown as number, {
+          name: name,
+          email: email,
+          password: password,
+        });
+
+      } catch (error) {
+
+        expect(error).toBeDefined();
+        expect(error).toHaveProperty('response');
+        expect(error.response).toHaveProperty('message', 'ID inválido.');
+        expect(error.response).toHaveProperty('error', 'Bad Request');
+        expect(error.response).toHaveProperty('statusCode', 400);
+
+      }
+
+    });
+
+    it('should return an error if user is not found', async () => {
+
+      try {
+
+        await service.update(5, {
+          name: name,
+          email: email,
+          password: password,
+        });
+
+      } catch (error) {
+
+        expect(error).toBeDefined();
+        expect(error).toHaveProperty('response');
+        expect(error.response).toHaveProperty('message', 'Usuário não encontrado.');
+        expect(error.response).toHaveProperty('error', 'Not Found');
+        expect(error.response).toHaveProperty('statusCode', 404);
+
+      }
+
+    });
+
+  });
+
+  describe('remove method', () => {
+
+    it('should remove an user', async () => {
+
+      const removedUser = await service.remove(1);
+
+      expect(removedUser).toBeDefined();
+      expect(removedUser).toHaveProperty('id');
+
+    });
+
+    it('should return an error if id is invalid', async () => {
+
+      try {
+
+        await service.remove('ABC' as unknown as number);
+
+      } catch (error) {
+
+        expect(error).toBeDefined();
+        expect(error).toHaveProperty('response');
+        expect(error.response).toHaveProperty('message', 'ID inválido.');
+        expect(error.response).toHaveProperty('error', 'Bad Request');
+        expect(error.response).toHaveProperty('statusCode', 400);
+
+      }
+
+    });
+
+    it('should return an error if user is not found', async () => {
+
+      try {
+
+        await service.remove(2);
+
+      } catch (error) {
+
+        expect(error).toBeDefined();
+        expect(error).toHaveProperty('response');
+        expect(error.response).toHaveProperty('message', 'Usuário não encontrado.');
+        expect(error.response).toHaveProperty('error', 'Not Found');
+        expect(error.response).toHaveProperty('statusCode', 404);
 
       }
 
